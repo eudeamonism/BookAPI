69b32fcc0a61a3ee3f029c6331d32df5b9d91bb5
Evan Vargas <evanvargas@gmail.com>
2020-12-15 21:05:09 -0600

    We replaced ZZZss with YYYs

--- BookAPI/Controllers/WeatherForecastController.cs
+++ BookAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace BookAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "ZZZZ", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "YYYY", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
