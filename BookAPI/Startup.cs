b97f2571fbf78168ed9466f9792977a6498443a9
Evan Vargas <evanvargas@gmail.com>
2020-12-21 11:17:53 -0600

    Difficulty with Startup.cs since it already has an Iconfguration; lecture 20

--- BookAPI/Startup.cs
+++ BookAPI/Startup.cs
@@ -14,6 +14,7 @@ namespace BookAPI
 {
     public class Startup
     {
+        //public static IConfiguration Configuration { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
