f095917082fde78c0d048bb9bb52ad3240314acf
Evan Vargas <evanvargas@gmail.com>
2021-01-10 10:19:44 -0600

    Added ICountryRepository & ICountryRepository

---
+++ BookAPI/Services/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiProject.Services
+{
+    public class ICategoryRepository
+    {
+    }
+}
