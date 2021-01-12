6c7639e115a81e7b22bcb51097b4a6ee4cebcc62
Evan Vargas <evanvargas@gmail.com>
2021-01-11 20:14:23 -0600

    updated repositories

--- BookAPI/Services/CategoryRepository.cs
+++ BookAPI/Services/CategoryRepository.cs
@@ -16,12 +16,12 @@ namespace BookApiProject.Services
         }
         public bool CategoryExists(int categoryId)
         {
-            throw new NotImplementedException();
+            return _categoryContext.Categories.Any(c => c.Id == categoryId);
         }
 
         public ICollection<Book> GetAllBooksForCategory(int categoryId)
         {
-            throw new NotImplementedException();
+            return _categoryContext.BookCategories.Where(c => c.CategoryId == categoryId).Select(b => b.Book).ToList();
         }
 
         public ICollection<Category> GetCategories()
@@ -29,14 +29,19 @@ namespace BookApiProject.Services
             return _categoryContext.Categories.OrderBy(c => c.Name).ToList();
         }
 
-        public ICollection<Category> GetCategoriesForABook(int bookId)
+        public ICollection<Category> GetAllCategoriesForABook(int bookId)
         {
-            throw new NotImplementedException();
+            return _categoryContext.BookCategories.Where(b => b.BookId == bookId).Select(c => c.Category).ToList();
         }
 
         public Category GetCategory(int categoryId)
         {
             return _categoryContext.Categories.Where(c => c.Id == categoryId).FirstOrDefault();
+        }
+
+        public ICollection<Category> GetCategoriesForABook(int bookId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
