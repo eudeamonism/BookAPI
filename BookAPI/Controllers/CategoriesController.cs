119d1fabf0e6ce3fc44dabd27aae58b614a15a9b
Evan Vargas <evanvargas@gmail.com>
2021-01-13 20:20:03 -0600

    CategoryController

---
+++ BookAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using BookApiProject.Dtos;
+using BookApiProject.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+        private ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        //api/categories
+        [HttpGet]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<CategoryDto>))]
+        public IActionResult GetCategories()
+        {
+            var categories = _categoryRepository.GetCategories();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var categoriesDto = new List<CategoryDto>();
+            foreach (var category in categories)
+            {
+                categoriesDto.Add(new CategoryDto
+                {
+                    Id = category.Id,
+                    Name = category.Name
+                });
+            }
+
+            return Ok(categoriesDto);
+        }
+    }
+}
