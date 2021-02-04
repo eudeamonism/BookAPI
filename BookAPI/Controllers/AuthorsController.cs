98d73b3229959270fbf3c1f00cbeb84ff1c755b0
Evan Vargas <evanvargas@gmail.com>
2021-02-03 19:21:45 -0600

    AuthorsController

---
+++ BookAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,45 @@
+﻿using BookApiProject.Dtos;
+using BookApiProject.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : Controller
+    {
+        private IAuthorRepository _authorRepository;
+        public AuthorsController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+        //api/authors
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetAuthors()
+        {
+            var authors = _authorRepository.GetAuthors();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var authorsDto = new List<AuthorDto>();
+            foreach (var author in authors)
+            {
+                authorsDto.Add(new AuthorDto
+                {
+                    Id = author.Id,
+                    FirstName = author.FirstName,
+                    LastName = author.LastName
+                });
+            }
+            return Ok(authorsDto);
+
+        }
+    }
+}
