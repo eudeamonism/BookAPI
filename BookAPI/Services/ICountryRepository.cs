65f9c9d3e3d8590418bbc07671b3b530bbbf1605
Evan Vargas <evanvargas@gmail.com>
2021-02-18 11:44:03 -0600

    IcountryRepository Methods

--- BookAPI/Services/ICountryRepository.cs
+++ BookAPI/Services/ICountryRepository.cs
@@ -14,5 +14,9 @@ namespace BookApiProject.Services
         ICollection<Author> GetAuthorsFromACountry(int countryId);
         bool CountryExists(int countryId);
         bool IsDuplicateCountryName(int countryId, string countryName);
+        bool CreateCountry(Country country);
+        bool UpdateCountry(Country country);
+        bool DeleteCountry(Country country);
+        bool Save();
     }
 }
