2696ccd2f16cb815cf7d419215fc0ca6793d7fdd
Evan Vargas <evanvargas@gmail.com>
2021-03-12 20:40:51 -0600

    DeleteReviewer

--- BookAPI/Services/IReviewRepository.cs
+++ BookAPI/Services/IReviewRepository.cs
@@ -16,6 +16,7 @@ namespace BookApiProject.Services
         bool CreateReview(Review review);
         bool UpdateReview(Review review);
         bool DeleteReview(Review review);
+        bool DeleteReviews(List<Review> reviews);
         bool Save();
     }
 }
